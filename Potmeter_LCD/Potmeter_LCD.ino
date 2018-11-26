1fb1a17b8943b9839a6c2b2927dafdcb286c9c7f
Jos <jostiexd@gmail.com>
2018-11-26 11:55:49 +0100

    Just a white screen

--- Potmeter_LCD/Potmeter_LCD.ino
+++ Potmeter_LCD/Potmeter_LCD.ino
@@ -1,6 +1,25 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+#include <Adafruit_STMPE610.h>
+#include <avr/io.h>
 
+#define TFT_DC 9
+#define TFT_CS 10
+#define STMPE_CS 8
+
+Adafruit_STMPE610 ts = Adafruit_STMPE610(STMPE_CS);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 int main(void)
 {
+	init();
+
+	//Scherm setup:
+	tft.begin();
+	tft.fillScreen(ILI9341_WHITE);
 	
+	while(1)
+	{
+
+	}
 }
