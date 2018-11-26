b7fec422047866399168320f984ec14784ac36ae
Jos <jostiexd@gmail.com>
2018-11-26 11:41:01 +0100

    Clearness LCD
    Controlled with potmeter

---
+++ Potmeter_LCD/Potmeter_LCD.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       Potmeter_LCD.ino
+    Created:	26-11-2018 11:38:04
+    Author:     LAPTOP-2MU7L2VG\Jos
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
