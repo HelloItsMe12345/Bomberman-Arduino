e18a54a0389d3e6ef0c9e4eb427b839f441671f1
Jos <jostiexd@gmail.com>
2018-11-26 15:30:01 +0100

    Screen is blinking

--- Potmeter_LCD/Potmeter_LCD.ino
+++ Potmeter_LCD/Potmeter_LCD.ino
@@ -1,8 +1,16 @@
+#include <Timer1/TimerOne.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 #include <Adafruit_STMPE610.h>
 #include <avr/io.h>
 
+//Define pin 3
+#define BL_FULL		PORTD |= (1 << PORTD3)
+#define BL_OFF		PORTD &= ~(1 << PORTD3)
+#define BL_TOGGLE	PIND |= (1 << PIND3)
+
+
+//Define LCD
 #define TFT_DC 9
 #define TFT_CS 10
 #define STMPE_CS 8
@@ -10,16 +18,28 @@
 Adafruit_STMPE610 ts = Adafruit_STMPE610(STMPE_CS);
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
+void callback()		//Interrupt van Timer1
+{
+	Timer1.pwm(9, 128);
+}
+
 int main(void)
 {
 	init();
+	
+	//Setup Timer1
+	Timer1.initialize(1000000);
+	Timer1.attachInterrupt(callback);
+	DDRD   |= (1 << DDD3);
 
-	//Scherm setup:
+	//Setup LCD
 	tft.begin();
 	tft.fillScreen(ILI9341_WHITE);
+	BL_FULL;
+	
+	sei();
 	
 	while(1)
 	{
-
 	}
 }
